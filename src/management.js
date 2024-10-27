4b3e76b4d675ac8a4ea84699b4aa3c450a969595
amagallanes15 <123508449+amagallanes15@users.noreply.github.com>
2024-10-27 15:06:10 -0600

    Added the update event form and management functionality, and design.

--- src/management.js
+++ src/management.js
@@ -1,10 +1,11 @@
 import app from './config.js';
-import { getFirestore, collection, addDoc, doc, deleteDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 const db = getFirestore();
 
 const addEventForm = document.getElementById("addEventForm");
 const cancelEventForm = document.getElementById("cancelEventForm");
+const updateEventForm = document.getElementById("updateEventForm");
 
 
 addEventForm.addEventListener('submit', async (e) => {
@@ -46,5 +47,25 @@ cancelEventForm.addEventListener('submit', async (e) => {
     } catch (error) {
         console.error("Error canceling event: ", error);
         alert('Failed to cancel event.');
+    }
+});
+
+updateEventForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const eventId = updateEventForm.event_id.value;
+    const newDate = updateEventForm.date.value;
+    const newTime = updateEventForm.time.value;
+    console.log("Update event ID: ", eventId);
+
+    const eventRef = doc(db, 'Events', eventId);
+    try {
+        await updateDoc(eventRef, {
+            date: newDate,
+            time: newTime
+        });
+        alert('Event updated successfully!');
+    } catch (error) {
+        console.error("Error updating event: ", error);
+        alert('Failed to update event. Please check the event ID.');
     }
 });
